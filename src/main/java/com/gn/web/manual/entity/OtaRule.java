f0d0896c861c2e10009cb6e74462ddf6ac80d796
gwk1123 <g15625281551@163.com>
2021-02-01 18:00:33 +0800

    规则

--- src/main/java/com/gn/web/manual/entity/OtaRule.java
+++ src/main/java/com/gn/web/manual/entity/OtaRule.java
@@ -25,23 +25,14 @@ public class OtaRule implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "OTA平台代码 CTRIP-携程 FLIGGY-飞猪 QUNAR-去哪儿 LY-同程 WAP133-航班管家 JIULVXING-就旅行 MEITUAN-美团")
-    private String otaCode;
-
-    @ApiModelProperty(value = "OTA平台中文")
-    private String otaCname;
-
     @ApiModelProperty(value = "OTA站点代码")
     private String otaSiteCode;
-
-    @ApiModelProperty(value = "OTA站点中文")
-    private String otaSiteCname;
 
     @ApiModelProperty(value = "航司")
     private String airline;
 
     @ApiModelProperty(value = "ota规则类型OTA-1,OTA-2")
-    private String otaRuleType;
+    private String ruleType;
 
     @ApiModelProperty(value = "出发机场码 多个用英文的/分开 999表示全国")
     private String depAirport;
@@ -51,9 +42,6 @@ public class OtaRule implements Serializable {
 
     @ApiModelProperty(value = "舱位 多个用英文的/分开")
     private String cabin;
-
-    @ApiModelProperty(value = "适用类型 0-白名单 1-黑名单")
-    private Boolean applyType;
 
     @ApiModelProperty(value = "自定义内容一")
     private String parameter1;
@@ -79,9 +67,6 @@ public class OtaRule implements Serializable {
     @ApiModelProperty(value = "自定义内容八")
     private String parameter8;
 
-    @ApiModelProperty(value = "id")
-    private Long id;
-
     @ApiModelProperty(value = "版本控制")
     private Integer version;
 
@@ -90,27 +75,6 @@ public class OtaRule implements Serializable {
 
     @ApiModelProperty(value = "优先排序序号")
     private Integer sortSeq;
-
-    @ApiModelProperty(value = "状态：0-正常,1-暂停(挂起),99-无效(删除)")
-    private Integer status;
-
-    @ApiModelProperty(value = "创建时间")
-    private Date createTime;
-
-    @ApiModelProperty(value = "最后修改时间")
-    private Date updateTime;
-
-    @ApiModelProperty(value = "创建人ID")
-    private Long createUserId;
-
-    @ApiModelProperty(value = "创建人名称")
-    private String createUserName;
-
-    @ApiModelProperty(value = "修改人ID")
-    private Long updateUserId;
-
-    @ApiModelProperty(value = "修改人名称")
-    private String updateUserName;
 
     @ApiModelProperty(value = "数据来源 WEBSITE,B2B,FD,NFD,ML-自用")
     private String sourceType;
