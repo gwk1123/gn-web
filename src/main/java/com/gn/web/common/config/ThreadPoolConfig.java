6c9d20b31e7af68f9669ef350868613f56babc04
gwk1123 <g15625281551@163.com>
2021-02-17 22:05:15 +0800

    整合15

--- src/main/java/com/gn/web/common/config/ThreadPoolConfig.java
+++ src/main/java/com/gn/web/common/config/ThreadPoolConfig.java
@@ -19,10 +19,10 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ThreadPoolConfig
 {
     // 核心线程池大小
-    private int corePoolSize = 40;
+    private int corePoolSize = 60;
 
     // 最大可创建的线程数
-    private int maxPoolSize = 50;
+    private int maxPoolSize = 80;
 
     // 队列最大长度
     private int queueCapacity = 100000;
